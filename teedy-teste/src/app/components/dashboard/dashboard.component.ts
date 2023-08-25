fcc14216cf5a0199e9af0b8c1c912411f8949d25
ArthurAthayde <arthur.athaydee@gmail.com>
2023-08-25 00:26:05 -0300

    feat: getAll with observable logic

--- teedy-teste/src/app/components/dashboard/dashboard.component.ts
+++ teedy-teste/src/app/components/dashboard/dashboard.component.ts
@@ -3,74 +3,82 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { PartnersService } from 'src/app/services/partners.service';
 import { IPartner } from 'src/app/interfaces/IPartners';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule],
+  imports: [MatButtonModule, MatMenuModule, CommonModule],
 })
 export class DashboardComponent {
+  partners: IPartner[] = [];
+
   constructor(private partnersService: PartnersService) {}
 
   getAllPartners() {
     this.partnersService
       .getAll()
-      .then((partners) => console.log(partners))
-      .catch((error) => console.error(error));
+      .subscribe((partners) => (this.partners = partners));
   }
+  // getAllPartners() {
+  //   this.partnersService
+  //     .getAll()
+  //     .then((partners) => console.log(partners))
+  //     .catch((error) => console.error(error));
+  // }
 
-  getPartnerById() {
-    this.partnersService
-      .getById('3')
-      .then((partners) => console.log(partners))
-      .catch((error) => console.error(error));
-  }
+  // getPartnerById() {
+  //   this.partnersService
+  //     .getById('3')
+  //     .then((partners) => console.log(partners))
+  //     .catch((error) => console.error(error));
+  // }
 
-  createNewPartner() {
-    const partner: IPartner = {
-      name: 'Nome do parceiro',
-      description: 'Para que serve essa parceria',
-      repositoryGit: 'Link do repositório do git',
-      urlDoc: 'Link da documentação do parceiro',
-      clients: ['Banco da Galera', 'Alegria Bankers'],
-      projects: ['Coban', 'Teddy360'],
-    };
+  // createNewPartner() {
+  //   const partner: IPartner = {
+  //     name: 'Nome do parceiro',
+  //     description: 'Para que serve essa parceria',
+  //     repositoryGit: 'Link do repositório do git',
+  //     urlDoc: 'Link da documentação do parceiro',
+  //     clients: ['Banco da Galera', 'Alegria Bankers'],
+  //     projects: ['Coban', 'Teddy360'],
+  //   };
 
-    this.partnersService
-      .createPartner(partner)
-      .then((partners) =>
-        console.log(`${partners?.name} has been successfully registered`)
-      )
-      .catch((error) => console.error(error));
-  }
+  //   this.partnersService
+  //     .createPartner(partner)
+  //     .then((partners) =>
+  //       console.log(`${partners?.name} has been successfully registered`)
+  //     )
+  //     .catch((error) => console.error(error));
+  // }
 
-  putPartner() {
-    const partner: IPartner = {
-      id: '10',
-      name: 'Novo nome',
-      description: 'Para que serve essa parceria',
-      repositoryGit: 'Link do repositório do git',
-      urlDoc: 'Link da documentação do parceiro',
-      clients: ['Banco da Galera', 'Alegria Bankers'],
-      projects: ['Coban', 'Teddy360'],
-    };
+  // putPartner() {
+  //   const partner: IPartner = {
+  //     id: '10',
+  //     name: 'Novo nome',
+  //     description: 'Para que serve essa parceria',
+  //     repositoryGit: 'Link do repositório do git',
+  //     urlDoc: 'Link da documentação do parceiro',
+  //     clients: ['Banco da Galera', 'Alegria Bankers'],
+  //     projects: ['Coban', 'Teddy360'],
+  //   };
 
-    this.partnersService
-      .putPartnerInfo(partner)
-      .then((partners) =>
-        console.log(`${partners?.name} has been updated successfully`)
-      )
-      .catch((error) => console.error(error));
-  }
+  //   this.partnersService
+  //     .putPartnerInfo(partner)
+  //     .then((partners) =>
+  //       console.log(`${partners?.name} has been updated successfully`)
+  //     )
+  //     .catch((error) => console.error(error));
+  // }
 
-  deletePartner() {
-    this.partnersService
-      .deletePartner('91')
-      .then((partners) =>
-        console.log(`Partner has been deleted successfully`, partners)
-      )
-      .catch((error) => console.error(error));
-  }
+  // deletePartner() {
+  //   this.partnersService
+  //     .deletePartner('91')
+  //     .then((partners) =>
+  //       console.log(`Partner has been deleted successfully`, partners)
+  //     )
+  //     .catch((error) => console.error(error));
+  // }
 }
--- teedy-teste/src/app/services/partners.service.ts
+++ teedy-teste/src/app/services/partners.service.ts
@@ -7,36 +7,28 @@ import { IPartner } from '../interfaces/IPartners';
   providedIn: 'root',
 })
 export class PartnersService {
+  private apiUrl = environment.apiBaseUrl;
   constructor(private httpClient: HttpClient) {}
+
   getAll() {
-    const apiUrl = `${environment.apiBaseUrl}`;
-
-    return this.httpClient.get<IPartner[]>(`${apiUrl}`).toPromise();
+    return this.httpClient.get<IPartner[]>(this.apiUrl);
   }
-
-  getById(id: string) {
-    const apiUrl = `${environment.apiBaseUrl}`;
-
-    return this.httpClient.get<IPartner>(`${apiUrl}/${id}`).toPromise();
-  }
-
-  createPartner(partner: IPartner) {
-    const apiUrl = `${environment.apiBaseUrl}`;
-
-    return this.httpClient.post<IPartner>(`${apiUrl}`, partner).toPromise();
-  }
-
-  putPartnerInfo(partner: IPartner) {
-    const apiUrl = `${environment.apiBaseUrl}`;
-
-    return this.httpClient
-      .put<IPartner>(`${apiUrl}/${partner.id}`, partner)
-      .toPromise();
-  }
-
-  deletePartner(id: string) {
-    const apiUrl = `${environment.apiBaseUrl}`;
-
-    return this.httpClient.delete(`${apiUrl}/${id}`).toPromise();
-  }
+  // getById(id: string) {
+  //   const apiUrl = `${environment.apiBaseUrl}`;
+  //   return this.httpClient.get<IPartner>(`${apiUrl}/${id}`).toPromise();
+  // }
+  // createPartner(partner: IPartner) {
+  //   const apiUrl = `${environment.apiBaseUrl}`;
+  //   return this.httpClient.post<IPartner>(`${apiUrl}`, partner).toPromise();
+  // }
+  // putPartnerInfo(partner: IPartner) {
+  //   const apiUrl = `${environment.apiBaseUrl}`;
+  //   return this.httpClient
+  //     .put<IPartner>(`${apiUrl}/${partner.id}`, partner)
+  //     .toPromise();
+  // }
+  // deletePartner(id: string) {
+  //   const apiUrl = `${environment.apiBaseUrl}`;
+  //   return this.httpClient.delete(`${apiUrl}/${id}`).toPromise();
+  // }
 }
