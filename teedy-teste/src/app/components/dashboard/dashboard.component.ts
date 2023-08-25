d9ec32c78b5f7af874ffacb4161d83cd57d581b6
ArthurAthayde <arthur.athaydee@gmail.com>
2023-08-25 02:05:40 -0300

    feat: delete working 100%

--- teedy-teste/src/app/components/dashboard/dashboard.component.ts
+++ teedy-teste/src/app/components/dashboard/dashboard.component.ts
@@ -81,63 +81,10 @@ export class DashboardComponent {
     });
   }
 
-  // getAllPartners() {
-  //   this.partnersService
-  //     .getAll()
-  //     .then((partners) => console.log(partners))
-  //     .catch((error) => console.error(error));
-  // }
-
-  // getPartnerById() {
-  //   this.partnersService
-  //     .getById('3')
-  //     .then((partners) => console.log(partners))
-  //     .catch((error) => console.error(error));
-  // }
-
-  // createNewPartner() {
-  //   const partner: IPartner = {
-  //     name: 'Nome do parceiro',
-  //     description: 'Para que serve essa parceria',
-  //     repositoryGit: 'Link do repositório do git',
-  //     urlDoc: 'Link da documentação do parceiro',
-  //     clients: ['Banco da Galera', 'Alegria Bankers'],
-  //     projects: ['Coban', 'Teddy360'],
-  //   };
-
-  //   this.partnersService
-  //     .createPartner(partner)
-  //     .then((partners) =>
-  //       console.log(`${partners?.name} has been successfully registered`)
-  //     )
-  //     .catch((error) => console.error(error));
-  // }
-
-  // putPartner() {
-  //   const partner: IPartner = {
-  //     id: '10',
-  //     name: 'Novo nome',
-  //     description: 'Para que serve essa parceria',
-  //     repositoryGit: 'Link do repositório do git',
-  //     urlDoc: 'Link da documentação do parceiro',
-  //     clients: ['Banco da Galera', 'Alegria Bankers'],
-  //     projects: ['Coban', 'Teddy360'],
-  //   };
-
-  //   this.partnersService
-  //     .putPartnerInfo(partner)
-  //     .then((partners) =>
-  //       console.log(`${partners?.name} has been updated successfully`)
-  //     )
-  //     .catch((error) => console.error(error));
-  // }
-
-  // deletePartner() {
-  //   this.partnersService
-  //     .deletePartner('91')
-  //     .then((partners) =>
-  //       console.log(`Partner has been deleted successfully`, partners)
-  //     )
-  //     .catch((error) => console.error(error));
-  // }
+  deletePartner(partner: IPartner) {
+    this.partnersService.deletePartner(partner).subscribe(() => {
+      console.log('Parceiro deletado:', partner);
+      this.loadPartners();
+    });
+  }
 }
